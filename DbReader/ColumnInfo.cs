dbd5abaa432935219325124c0d917356fb09bd9f
Bernhard Richter <bernhard.richter@gmail.com>
2015-03-02 15:36:51 +0100

    Missing files

---
+++ DbReader/ColumnInfo.cs
@@ -0,0 +1,32 @@
+﻿namespace DbReader
+{
+    using System;
+
+    public class ColumnInfo
+    {
+        public ColumnInfo(int ordinal,string name,  Type type)
+        {
+            Ordinal = ordinal;
+            Name = name;
+            Type = type;
+        }
+
+        public int Ordinal { get; private set; }
+
+        public Type Type { get; private set; }
+
+        public string Name { get; private set; }
+
+        public override string ToString()
+        {
+            if (Ordinal == -1)
+            {
+                return "Not Mapped";
+            }
+            else
+            {
+                return "Name: {0}, Type: {1}, Ordinal: {2}".FormatWith(Name, Type, Ordinal);
+            }
+        }
+    }
+}
---
+++ DbReader/PropertyTypeValidator.cs
@@ -0,0 +1,36 @@
+﻿namespace DbReader
+{
+    using System;
+    using System.Data;
+    using System.Linq;
+
+    using DbReader.Interfaces;
+
+    public class PropertyTypeValidator : IPropertyMapper
+    {
+        private readonly IPropertyMapper propertyMapper;
+
+        public PropertyTypeValidator(IPropertyMapper propertyMapper)
+        {
+            this.propertyMapper = propertyMapper;
+        }
+
+        public MappingInfo[] Execute(Type type, IDataRecord dataRecord, string prefix)
+        {
+            var mappings = propertyMapper.Execute(type, dataRecord, prefix);
+            foreach (var mappingInfo in mappings.Where(m => m.ColumnInfo.Ordinal != -1))
+            {
+                if (!mappingInfo.Property.PropertyType.IsAssignableFrom(mappingInfo.ColumnInfo.Type))
+                {
+                    throw new InvalidOperationException(
+                        ErrorMessages.IncompatibleTypes.FormatWith(
+                            mappingInfo.Property,
+                            mappingInfo.ColumnInfo,
+                            mappingInfo.ColumnInfo.Type,
+                            mappingInfo.Property.PropertyType));
+                }
+            }
+            return mappings;
+        }
+    }
+}
